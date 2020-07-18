dc3f04c0f5048144a1279c3ca236b42a528f0f8f
Dawn Sheedy <sheedy.dawn@gmail.com>
2020-07-18 14:14:59 -0400

    updated social links

--- src/components/socials.js
+++ src/components/socials.js
@@ -3,10 +3,10 @@ import './../App.css'
 
 const Socials = (props) => {
     return (<div>
-        <span className="entry-anim"><a href="">GitHub</a> | </span>
-        <span className="entry-anim" style={{animationDelay:"150ms"}}><a href="">Twitter</a> | </span>
-        <span className="entry-anim" style={{animationDelay:"300ms"}}><a href="">Twitch</a> | </span>
-        <span className="entry-anim" style={{animationDelay:"450ms"}}><a href="">Youtube</a></span>
+        <span className="entry-anim"><a href="https://github.com/dawnsheedy">GitHub</a> | </span>
+        <span className="entry-anim" style={{animationDelay:"150ms"}}><a href="https://twitter.com/dawnshee_">Twitter</a> | </span>
+        <span className="entry-anim" style={{animationDelay:"300ms"}}><a href="https://twitch.tv/dawnshee">Twitch</a> | </span>
+        <span className="entry-anim" style={{animationDelay:"450ms"}}><a href="https://www.youtube.com/channel/UCsoFt8m8c9FWTb94m2gBEQA">Youtube</a></span>
     </div>);
 }
 
