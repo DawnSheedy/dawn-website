093247ec6688088132e459addd826abb376a7f4c
Dawn Sheedy <sheedy.dawn@gmail.com>
2021-06-07 00:34:55 -0400

    Resume swap, minor adjustments.
    Resume swap and discarded an experiment that was accidentally pushed.

--- src/App.js
+++ src/App.js
@@ -18,14 +18,6 @@ function App() {
           </Row>
         </Container>
       </div>
-      <Container>
-          <Row className="justify-content-md-left">
-            <Col sm={{ span: 12 }} md={{ span: 10, offset: 2 }} lg={{ span: 9, offset: 3 }}>
-              <Title />
-              <Socials />
-            </Col>
-          </Row>
-        </Container>
     </div>
   );
 }
--- src/components/socials.js
+++ src/components/socials.js
@@ -5,11 +5,13 @@ const Socials = (props) => {
     return (<div>
         <span className="entry-anim social-span" style={{animationDelay:"1000ms"}}><a href="mailto:contact@dawnsheedy.com">Contact</a></span>
         <div style={{animationDelay:"2500ms"}} className="vl"></div>
-        <span className="entry-anim social-span" style={{animationDelay:"1250ms"}}><a href={process.env.PUBLIC_URL + '/resume-dawn-2020.pdf' }>Resumé</a></span>
+        <span className="entry-anim social-span" style={{animationDelay:"1200ms"}}><a href={process.env.PUBLIC_URL + '/resume-dawn-2021.pdf' }>Resumé</a></span>
         <div style={{animationDelay:"2500ms"}} className="vl"></div>
-        <span className="entry-anim social-span" style={{animationDelay:"1500ms"}}><a href="https://github.com/dawnsheedy">GitHub</a></span>
+        <span className="entry-anim social-span" style={{animationDelay:"1400ms"}}><a href="https://github.com/dawnsheedy">GitHub</a></span>
         <div style={{animationDelay:"2500ms"}} className="vl"></div>
-        <span className="entry-anim social-span" style={{animationDelay:"1750ms"}}><a href="https://www.linkedin.com/in/dawn-sheedy-a641b780/">LinkedIn</a></span>
+        <span className="entry-anim social-span" style={{animationDelay:"1600ms"}}><a href="https://www.instagram.com/dawn.shee/">Instagram</a></span>
+        <div style={{animationDelay:"2500ms"}} className="vl"></div>
+        <span className="entry-anim social-span" style={{animationDelay:"1800ms"}}><a href="https://www.linkedin.com/in/dawn-sheedy-a641b780/">LinkedIn</a></span>
     </div>);
 }
 
