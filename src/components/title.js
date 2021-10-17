192c7556ccefdb90a2dc803cc601ca37955c1c60
Dawn Sheedy <sheedy.dawn@gmail.com>
2021-10-17 18:30:37 -0400

    Changed tagline

--- src/components/title.js
+++ src/components/title.js
@@ -5,7 +5,7 @@ const Title = (props) => {
     return (<div>
         <h1 className="App-headline"><span style={{animationDelay:"0ms"}} className="App-headline title-block entry-anim">Dawn</span><span style={{animationDelay:"150ms"}} className="App-headline title-block entry-anim">Sheedy</span></h1>
         <h1 className="App-subline"><span style={{animationDelay:"300ms"}} className="App-subline title-block entry-anim">(she/her)</span></h1>
-        <span style={{animationDelay:"450ms"}} className="subtitle entry-anim">Software Engineer // Artist // Gamer</span>
+        <span style={{animationDelay:"450ms"}} className="subtitle entry-anim">Software Engineer // Artist</span>
         <span style={{animationDelay:"600ms"}} className="subtitle entry-anim">I make (and break) web apps.</span>
         <hr style={{animationDelay:"600ms"}} className="expand-anim"/>
     </div>);
