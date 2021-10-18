df4189481ea8bc6f480f88b45a8904eb4c4f41df
Dawn Sheedy <sheedy.dawn@gmail.com>
2021-10-17 22:29:42 -0400

    redo animations for social collapse

--- src/components/socialLink.js
+++ src/components/socialLink.js
@@ -7,6 +7,7 @@ const SocialLink = (props) => {
         <div style={{animationDelay:Math.max(delay-100, 0)+"ms"}} className="vl"></div>
         <div style={{animationDelay:Math.max(delay-100, 0)+"ms"}} className="vl"></div>
         <span className="entry-anim social-span" style={{animationDelay: delay+"ms"}}><a href={props.link}>{props.text}</a></span> 
+        {props.children}
     </div>);
 }
 
--- src/components/socials.js
+++ src/components/socials.js
@@ -4,25 +4,35 @@ import SocialLink from './socialLink';
 
 const Socials = (props) => {
     const [show, setShow] = useState(false);
+    const [firstShow, setFirst] = useState(true);
 
-    if (show) {
-        return (<div class="social-link-container open">
+    function socialLinks() {
+        if (show) {
+            return (<>
             <SocialLink link="mailto:contact@dawnsheedy.com" text="Contact" delay={generateRandomDelay()} />
             <SocialLink link="https://www.linkedin.com/in/dawnshee" text="LinkedIn" delay={generateRandomDelay()} />
             <SocialLink link="https://github.com/dawnsheedy" text="GitHub" delay={generateRandomDelay()} />
             <SocialLink link="https://twitter.com/dawnshee_" text="Twitter" delay={generateRandomDelay()} />
             <SocialLink link="https://instagram.com/dawn.shee" text="Instagram" delay={generateRandomDelay()} />
-            <SocialLink link={process.env.PUBLIC_URL + '/resume-dawn-2021.pdf' } text="Resumé" delay={generateRandomDelay()} />
-        </div>);
-    } else {
-        return (<div class="social-link-container closed">
-            <div class={"social-line "+props.className}>
-                <div style={{animationDelay:"900ms"}} className="vl"></div>
-                <div style={{animationDelay:"900ms"}} className="vl"></div>
-                <span className="entry-anim social-span" style={{animationDelay: "500ms"}}><a href="#" onClick={() => {setShow(true)}}>more_info</a></span> 
-            </div>
-        </div>)
+            <SocialLink link={process.env.PUBLIC_URL + '/resume-dawn-2021.pdf' } text="Resumé" delay={generateRandomDelay()}>
+                <div class="cursor"></div>
+            </SocialLink>
+            </>)
+        } else {
+            return (<></>)
+        }
     }
+
+    return (<div class={"social-link-container "+(firstShow?"":show?"open":"closed")}>
+        <div class={"social-line "+props.className}>
+            <div style={{animationDelay:(firstShow?"900ms":"0ms")}} className={"vl no-indent "+(firstShow?"":show?"expanded top":"collapsed top")}></div>
+            <div style={{animationDelay:(firstShow?"900ms":"0ms")}} className={"vl no-indent "+(firstShow?"":show?"expanded":"collapsed")}></div>
+            <span className="social-span no-indent entry-anim" style={{animationDelay:"500ms"}}><a href="#" onClick={() => {setShow(!show); setFirst(false)}}>{show?"collapse_social_info":"expand_social_info"}</a></span> 
+            {!show?<div class="cursor"></div>:<></>}
+        </div>
+        {socialLinks()}
+        </div>);
+    
 }
 
 function generateRandomDelay() {
