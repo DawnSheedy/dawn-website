0def80feef64a325db751cec664b647d63f039d6
Dawn Sheedy <sheedy.dawn@gmail.com>
2023-02-06 11:02:01 -0800

    switch to hashrouter for github pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import "./sharedStyles/animations.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
