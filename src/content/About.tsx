2aa863bf5b9ddca0767d416ad5407dd16457d24e
Dawn Sheedy <dawn@dawnsheedy.com>
2026-07-08 17:52:27 -0700

    Update resume and about content, remove package-lock.json

--- src/content/About.tsx
+++ src/content/About.tsx
@@ -8,14 +8,16 @@ export const About = () => {
       <SectionHeading id="about-section-heading">About Me</SectionHeading>
       <p>
         I'm Dawn Sheedy, a professional software engineer specializing in
-        full-stack web development. I have about {yearsSinceStart} years of
+        full-stack web services development. I have about {yearsSinceStart} years of
         professional working experience, but I've had a vested interest in
         technology for as long as I can remember.
       </p>
       <br />
       <p>
-        Since July 2022, I've worked for Clearwater Analytics in Seattle, WA. My
-        current job title is Software Development Engineer III.
+        Since July 2022, I've worked for CWAN (Clearwater Analytics) in Seattle, WA. My
+        current job title is Software Development Engineer III. At CWAN, I am currently
+        working on the Customer Identity and Access Management team, but I previously
+        worked on alternative investment solutions.
       </p>
     </section>
   );
